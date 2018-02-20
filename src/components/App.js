8b6f86ee0c378aef3c9cdcd71f2c9b203904d7a7
ivan limongan <ivan.limongan@gmail.com>
2018-02-19 22:17:24 -0800

    basic jsx structure created, had to wrap in an overall div

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,40 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+
+    };
+  }
+
   render() {
+    const { } = this.state;
+
 
     return (
-      <h1>test</h1>
+      <div>
+        <header role="banner" id="header">
+          <h1>Meme Generator</h1>
+        </header>
+
+        <section role="main" id="main">
+
+          <section>
+            Controls:
+          </section>
+
+          <section>
+            output
+          </section>
+
+        </section>
+
+        <footer role="contentinfo" id="footer">
+          <p>(c) Meme Generator | <a href="https://github.com/limongoo/meme-generator/tree/ivan" target="_blank" rel="author noopener noreferrer">Ivan Limongan</a></p>
+        </footer>
+      </div>
     );
   }
 }
